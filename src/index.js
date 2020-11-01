cf25facee1075597a5fdd339b1f8d05fa9cf7624
zilhamdev <70104357+zilhamdev@users.noreply.github.com>
2020-11-01 11:58:27 +0700

    try render div

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsm dolor sit amet cadukloro telu.</p>
+  </div>,
+  document.getElementById("root")
+);
